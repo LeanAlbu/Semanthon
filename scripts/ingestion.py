d9f196fad5ae32d1e1c68ea071af280b0c528c4b
Lean Albuquerque <lean.albuquerque@mail.uft.edu.br>
2026-05-13 13:35:57 -0300

    feat(script)Adds ingestion process

---
+++ scripts/ingestion.py
@@ -0,0 +1,16 @@
+from scripts import logCleaner
+from crud import repository
+
+def Database_Ingestion():
+    print(f"Iniciando ingestão dos logs")
+    dataframe = logCleaner.Criar_Vetor()
+    print(f"Encontrados:{len(dataframe)} logs...")
+
+    for index, row in dataframe.iterrows():
+        repository.Database_add(
+            str(row['texto_para_embedding']),
+            str(row['timestamp']),
+            str(row['source_ip'])
+        )
+
+        print("Ingestão concluída")
